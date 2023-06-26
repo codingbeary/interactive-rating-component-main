8f986008b1bcbb9e4de6005f5d27f29c48978bdb
kiyooteru <kiyooteru@gmail.com>
2023-06-26 16:29:57 +0200

    Implemented content update

--- index.js
+++ index.js
@@ -3,21 +3,36 @@
 let ratingBackgroundElements =
   document.getElementsByClassName("rating-background");
 
+let selectedRating = null;
+
 for (let i = 0; i < ratingBackgroundElements.length; i++) {
-  ratingBackgroundElements[i].addEventListener("click", changeColor);
+  ratingBackgroundElements[i].addEventListener("click", selectRating);
 }
 
-function changeColor() {
+function selectRating() {
   for (let i = 0; i < ratingBackgroundElements.length; i++) {
     ratingBackgroundElements[i].classList.remove("changedColor");
   }
   this.classList.add("changedColor");
+
+  selectedRating = parseInt(this.getAttribute("data-rating"));
 }
 
 let button = document.getElementById("btn");
 
-button.addEventListener("click", thankYouNote);
+button.addEventListener("click", updateContent);
 
-function thankYouNote() {
-  alert("hello");
+function updateContent() {
+  if (selectedRating !== null) {
+    let mainContent = document.getElementById("mainContent");
+    mainContent.classList.add("hidden");
+
+    let thankYouContent = document.getElementById("thankYouContent");
+    thankYouContent.classList.remove("hidden");
+
+    let chosenRating = document.getElementById("chosenRating");
+    chosenRating.innerHTML = selectedRating;
+  } else {
+    alert("Select a rating.");
+  }
 }
